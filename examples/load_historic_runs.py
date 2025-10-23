e8515da7f52b5f2a10b64690165e80c4c58c33cb
Matt Field <matthew.field@ukaea.uk>
2025-10-23 14:29:14 +0100

    Refactor examples and dockerfile

--- examples/load_historic_runs.py
+++ examples/load_historic_runs.py
@@ -9,40 +9,38 @@ with supply and exhaust air vents activating after a few seconds to remove the s
 This example does not require installation of FDS, as the results have already been generated.
 View the results in the folder `examples/example_results`.
 
-To run this example:
+To run this example with Docker:
+    - Pull the FDS image: docker run -it ghcr.io/simvue-io/fds_example
+    - Create a simvue.toml file, copying in your information from the Simvue server: nano simvue.toml
+    - Run the example script: python load_historic_runs.py
+
+To run this example on your own system:
     - Clone this repository: git clone https://github.com/simvue-io/connectors-fds.git
-    - Move into FDS examples directory: cd connectors-fds/examples
+    - Move into FDS examples directory: cd connectors-fds/examples/fds
     - Create a simvue.toml file, copying in your information from the Simvue server: vi simvue.toml
-    - Install Poetry: pip install poetry
-    - Install required modules: poetry install
-    - Run the example script: poetry run python load_historic_runs.py
+    - Create a venv: python -m venv venv
+    - Activate the venv: `source venv/bin/activate` on UNIX, or `venv/Scripts/activate` on Windows
+    - Install required modules: pip install .
+    - Run the example script: poetry run python load_historic_results.py
     
 For a more in depth example, see: https://docs.simvue.io/examples/fds/
 """
-
 import pathlib
-import shutil
-import uuid
 from simvue_fds.connector import FDSRun
 
-
-# Initialise the FDSRun class as a context manager
+# Use the FDSRun class as a context manager
 with FDSRun() as run:
-    # Initialise the run, providing a name for the run, and optionally extra information such as a folder, description, tags etc
+    # Initialize the run with a name, and optional other parameters
     run.init(
-        name="fds_simulation_vents-%s" % str(uuid.uuid4()),
-        description="An example of using the FDSRun Connector to load an FDS simulation.",
-        folder="/simvue_client_examples/fds",
-        tags=["fds", "vents"],
-    )
+        name="fds_simulation_stronger_vents",
+        folder="/examples/fds"
+        )
 
-    # Call the .load() method to load historic results, providing the path to the results directory
+    # Call any other simvue Run() methods, such as adding tags or alerts
+    run.update_tags(["fds", "vents"])
+
+    # Call run.load(), providing the path to your directory of results
     run.load(
-        results_dir = pathlib.Path(__file__).parent.joinpath("example_results"),
-        # You can optionally have the connector track slices in your simulation
-        slice_parse_quantity = "TEMPERATURE",
+        results_dir=pathlib.Path(__file__).parent.joinpath("example_results"),
+        slice_parse_quantity = "TEMPERATURE"
     )
-    
-    # Once the simulation is complete, you can upload any final items to the Simvue run before it closes
-    run.log_event("Finished uploading results stored in directory 'example_results'!")
-
