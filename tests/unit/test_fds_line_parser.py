d842f1b18a1beb4e0ce41c3e0191de779fdb4925
Matt Field <matthew.field@ukaea.uk>
2025-11-03 18:00:42 +0000

    Start adding metrics to tests for line parser

--- tests/unit/test_fds_line_parser.py
+++ tests/unit/test_fds_line_parser.py
@@ -1,19 +1,11 @@
 from simvue_fds.connector import FDSRun
-import simvue
 import pathlib
-import shutil
-import time
-import uuid
-import tempfile
-import threading
-from unittest.mock import patch
-import pytest
-import shutil
 import f90nml
-
-def parse_line_data(run, input_path):
+import requests
+from simvue.config.user import SimvueConfiguration
+def parse_line_data(run, scenario_name):
         # Load input file as data (normally done by Multiparser)
-        nml = f90nml.read(input_path).todict()
+        nml = f90nml.read(pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", f"{scenario_name}.fds")).todict()
         run._input_dict = {k: v for k, v in nml.items() if v}
         
         # Add this which is typically added in launch()
@@ -21,78 +13,106 @@ def parse_line_data(run, input_path):
         
         # Generate mapping
         run._map_line_var_coords()
+        
+def extract_line_metrics(run, scenario_name):
+    meta, data = run._line_parser(str(pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", f"{scenario_name}.csv")))
+    run._line_callback(data, meta)
+    
+def get_line_metrics(run_id, metric):
+    # Check slice uploaded as 3D metric
+    _user_config: SimvueConfiguration = SimvueConfiguration.fetch()
+    response = requests.get(
+        url=f"{_user_config.server.url}/runs/{run_id}/metrics/{metric}/values?step=0",
+        headers={
+            "Authorization": f"Bearer {_user_config.server.token.get_secret_value()}",
+            "User-Agent": "Simvue Python client",
+            "Accept-Encoding": "gzip",
+        }
+    )
+    assert response.status_code == 200
+    return response.json()
 
 def test_single_devc_xb():
+    scenario_name = "single_devc_xb"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "single_devc_xb.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check device ID has been mapped to correct axis label (default: temp_line-y)
         assert run._line_var_coords["temp_line"] == "temp_line-y"
         
 def test_single_devc_xbp():
+    scenario_name = "single_devc_xbp"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "single_devc_xbp.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check device ID has been mapped to correct axis label (default: temp_line-y)
         assert run._line_var_coords["temp_line"] == "temp_line-y"
         
 def test_single_devc_coord_id():
+    scenario_name = "single_devc_coord_id"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "single_devc_coord_id.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check device ID has been mapped to correct axis label (user specified: y_coords)
         assert run._line_var_coords["temp_line"] == "y_coords"
         
         
 def test_single_devc_multi_dimension():
+    scenario_name = "single_devc_multi_dimension"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "single_devc_multi_dimension.fds"))
+        parse_line_data(run, scenario_name)
         
         # Only supports 1D line devices for now - so this should report None, indicating that this device should be skipped when adding metrics
         assert run._line_var_coords["temp_line"] == None
         
 def test_single_devc_no_id():
+    scenario_name = "single_devc_no_id"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "single_devc_no_id.fds"))
+        parse_line_data(run, scenario_name)
         
         # Should skip devices which have no ID provided
         assert run._line_var_coords == {}
         
 def test_multi_devc():
+    scenario_name = "multi_devc"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_devc.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check both devices mapped to their default values
         assert run._line_var_coords["temp_line_y"] == "temp_line_y-y"
         assert run._line_var_coords["temp_line_x"] == "temp_line_x-x"
         
 def test_multi_devc_coord_id():
+    scenario_name = "multi_devc_coord_id"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_devc_coord_id.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check both devices mapped to their user specified values
         assert run._line_var_coords["temp_line_y"] == "y_coords"
         assert run._line_var_coords["temp_line_x"] == "x_coords"
         
 def test_multi_devc_hide_coords():
+    scenario_name = "multi_devc_hide_coords"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_devc_hide_coords.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check both devices are using default coord name from first defined device
         assert run._line_var_coords["temp_line_y"] == "temp_line_y-y"
         assert run._line_var_coords["velocity_line_y"] == "temp_line_y-y"
         
 def test_multi_devc_coord_id_hide_coords():
+    scenario_name = "multi_devc_coord_id_hide_coords"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_devc_coord_id_hide_coords.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check both devices are using user specified coord name from first defined device
         assert run._line_var_coords["temp_line_y"] == "y_coords"
         assert run._line_var_coords["velocity_line_y"] == "y_coords"
         
 def test_multi_devc_multi_hide_coords():
+    scenario_name = "multi_devc_multi_hide_coords"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_devc_multi_hide_coords.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check first two devices are using user specified 'y_coords'
         assert run._line_var_coords["temp_line_y"] == "y_coords"
@@ -103,8 +123,9 @@ def test_multi_devc_multi_hide_coords():
         assert run._line_var_coords["velocity_line_z"] == "temp_line_z-z"
         
 def test_multi_devc_hide_coords_invalid():
+    scenario_name = "multi_devc_hide_coords_invalid"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_devc_hide_coords_invalid.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check first two devices are using user specified 'y_coords'
         assert run._line_var_coords["temp_line_y"] == "y_coords"
@@ -120,8 +141,9 @@ def test_multi_devc_hide_coords_invalid():
         
         
 def test_multi_point_and_line_devc():
+    scenario_name = "multi_point_and_line_devc"
     with FDSRun() as run:
-        parse_line_data(run, pathlib.Path(__file__).parent.joinpath("example_data", "line_devices", "multi_point_and_line_devc.fds"))
+        parse_line_data(run, scenario_name)
         
         # Check point DEVC devices are not in mapping
         assert not run._line_var_coords.get("velocity_point")
