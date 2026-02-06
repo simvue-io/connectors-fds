47d9087f3a4b9746e039db20b0b2ee51a18a7a5a
Matt Field <matthew.field@ukaea.uk>
2026-02-06 10:38:44 +0000

    Add fix for tuple coords

--- simvue_fds/connector.py
+++ simvue_fds/connector.py
@@ -652,6 +652,7 @@ class FDSRun(WrappedRun):
             # We will just use the first one, since they should be almost identical...
             if isinstance(values, tuple):
                 values = values[0]
+                coords = coords[0]
 
             # Get rid of values already uploaded, return if nothing left to upload
             values = values[self._slice_processed_idx :, ...]
